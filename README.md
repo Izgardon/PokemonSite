ef22376b8090fd4a600c648d6c6a4ae58ed7980e
William Sessions <williamsessions23@gmail.com>
2023-12-19 10:34:32 +0000

    working on adding filters to work with search

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Pokemon site
 
+To run, pull down the repo and run 'npm install' inside the folder and then 'npm run serve' to open on port 8080.
+
+
+
+**Dev Notes**
 .env file is being pushed but would normally be hidden, just for ease of testing
--- src/app.js
+++ src/app.js
@@ -12,8 +12,15 @@ const paginationContainer = document.getElementById('paginationContainer');
 
 //Variables------------
 
+//Pokemon and associated data
 let pokemonList = [];
+let types = [];
+let colors = [];
+let regions = [];
+
+//Search delay
 let searchDelayTimer;
+//Pagination
 let currentPage = 1;
 let totalPages = 1;
 
@@ -22,36 +29,78 @@ let totalPages = 1;
 //Check to see if data is in cache, if not call api
 function checkCache() {
     pokemonList = JSON.parse(localStorage.getItem('pokemonData'));
-    if(!pokemonList){
+    types = JSON.parse(localStorage.getItem('pokemonTypes'));
+    colors = JSON.parse(localStorage.getItem('pokemonColors'));
+    regions = JSON.parse(localStorage.getItem('pokemonRegions'));
+    if(!pokemonList || !types || !regions || !colors){
         fetchPokemon()
     } else {
-        displayPokemon(pokemonList)
+        displayPokemon(pokemonList);
+        // Populate dropdowns
+        populateDropdown('typeDropdown', types);
+        populateDropdown('colorDropdown', colors);
+        populateDropdown('regionDropdown', regions);
+        addListenerToDropdowns();
     }
     searchBox.value = '';
 }
 
 // Fetching pokemon from API
 async function fetchPokemon(){
-     await axios.get(`${apiUrl}?limit=151`).then((resp) => {
+     await axios.get(`${apiUrl}/pokemon?limit=151`).then((resp) => {
         pokemonList = resp.data.results;
         pokemonList.forEach((pokemon) => {
             pokemon.image = getPokemonImageUrl(pokemon.url);
         });
         //Setting local storage
         localStorage.setItem('pokemonData', JSON.stringify(pokemonList));
-
+        //Getting filters
+        fetchFilters()
         //Displaying pokemon
         displayPokemon(pokemonList);
     }).catch((err) => {
         console.error('Error fetching Pokémon:', err);
-    }); 
+    });
 }
 
-// Get Pokémon image URL
-function getPokemonImageUrl(apiUrl) {
-    const pokemonId = apiUrl.split('/').slice(-2, -1)[0];
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+// Fetching filters
+async function fetchFilters(){
+    try {
+        // Fetch types
+        const typesResponse = await axios.get(`${apiUrl}/type`);
+        types = typesResponse.data.results;
+        localStorage.setItem('pokemonTypes', JSON.stringify(types));
+        console.log('hi')
+        // Fetch colors
+        const colorsResponse = await axios.get(`${apiUrl}/pokemon-color`);
+        colors = colorsResponse.data.results;
+        localStorage.setItem('pokemonColors', JSON.stringify(colors));
+
+        // Fetch regions
+        const regionsResponse = await axios.get(`${apiUrl}/region`);
+        regions = regionsResponse.data.results;
+        localStorage.setItem('pokemonRegions', JSON.stringify(regions));
+
+        // Populate dropdowns
+        populateDropdown('typeDropdown', types);
+        populateDropdown('colorDropdown', colors);
+        populateDropdown('regionDropdown', regions);
+        addListenerToDropdowns();
+    } catch (err) {
+        console.error('Error fetching filters:', err);
+    }
 }
+
+
+function handleDropdownChange(event) {
+    const selectedValue = event.target.value;
+    const dropdownId = event.target.id;
+
+    /* const apiEndpoint = apiEndpoints[dropdownId];
+
+    const apiUrl = `${apiEndpoint}${selectedValue}/`; */
+  }
+
 
 // Display Pokémon on the page using CSS Grid
 function displayPokemon(displayPokemonList, page = 1, itemsPerPage = 20) {
@@ -81,6 +130,18 @@ function displayPokemon(displayPokemonList, page = 1, itemsPerPage = 20) {
     } 
 }
 
+// Search functionality
+function handleSearch(page = 1) {
+    let searchInput = searchBox.value.toLowerCase();
+    if(searchInput && searchInput.length > 0){
+        let filteredPokemonList = pokemonList.filter(pokemon => pokemon.name.includes(searchInput.toLowerCase()));
+        displayPokemon(filteredPokemonList, page); 
+    } else {
+        displayPokemon(pokemonList, page);
+    }
+    
+}
+
 // Add pagination buttons
 function addPaginationButtons(currentPage) {
     const previousButton = createPaginationButton('Previous', currentPage - 1);
@@ -104,30 +165,25 @@ function createPaginationButton(label, page) {
     return button;
 }
 
-// Search delay
-function handleSearchWithDelay(page = 1) {
-    clearTimeout(searchDelayTimer);
-    searchDelayTimer = setTimeout(() => {
-        handleSearch(page);
-    }, 300);
+// Populate dropdown options
+function populateDropdown(dropdownId, options) {
+    const dropdown = document.getElementById(dropdownId);
+
+    dropdown.innerHTML = '<option value="">All</option>';
+
+    options.forEach((option) => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option.name;
+        optionElement.text = option.name;
+        dropdown.appendChild(optionElement);
+    });
 }
 
-// Search functionality
-function handleSearch(page = 1) {
-    let searchInput = searchBox.value.toLowerCase();
-    if(searchInput && searchInput.length > 0){
-        let filteredPokemonList = pokemonList.filter(pokemon => pokemon.name.includes(searchInput.toLowerCase()));
-        displayPokemon(filteredPokemonList, page); 
-    } else {
-        displayPokemon(pokemonList, page);
-    }
-    
-}
 
 // Open Modal and fetch more data
 async function openModal(pokemonName) {
     try {
-        const response = await axios.get(`${apiUrl}/${pokemonName}`);
+        const response = await axios.get(`${apiUrl}/pokemon/${pokemonName}`);
         const pokemonData = response.data;
 
         const modalHtml = `
@@ -147,16 +203,16 @@ async function openModal(pokemonName) {
     }
 }
 
+
+//Helpers------
+
 //Clicking outside modal closes it
 function addCloseModalEventListener(event){
     const isClickInsideModal = modalContent.contains(event.target) || event.target === modalContent;
     if (!isClickInsideModal) {
         closeModal();
-    }
-    
+    } 
 }
-
-//Helpers------
 
 // Close modal
 function closeModal() {
@@ -167,6 +223,30 @@ function closeModal() {
 // Capitalize
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Get Pokémon image URL
+function getPokemonImageUrl(url) {
+    const pokemonId = url.split('/').slice(-2, -1)[0];
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+}
+
+// Search delay
+function handleSearchWithDelay(page = 1) {
+    clearTimeout(searchDelayTimer);
+    searchDelayTimer = setTimeout(() => {
+        handleSearch(page);
+    }, 300);
+}
+
+function addListenerToDropdowns(){
+    const typeDropdown = document.getElementById('typeDropdown');
+    const colorDropdown = document.getElementById('colorDropdown');
+    const regionDropdown = document.getElementById('regionDropdown');
+
+    typeDropdown.addEventListener('change', handleTypeChange);
+    colorDropdown.addEventListener('change', handleColorChange);
+    regionDropdown.addEventListener('change', handleRegionChange);
 }
 
 // Initial fetch
